facbc30ceb2fbe8c7a172f2acc25a60dab57b1c3
pedro <pgrin@fi.uba.ar>
2025-03-16 20:34:01 -0300

    ej1

---
+++ generar-compose.py
@@ -0,0 +1,78 @@
+import sys
+
+def main(args):
+    file, clients_size = validate_args(args)
+    if file is None or clients_size is None:
+        return
+    
+    compose_file_lines = ["name: tp0", "services:"]
+    compose_file_lines += generate_server_lines()
+    compose_file_lines += generate_client_lines(clients_size)
+    compose_file_lines += generate_network_lines()
+
+    with open(file, "w") as f:
+        f.write("\n".join(compose_file_lines))
+    
+    
+def validate_args(args):
+    if len(args) != 3:
+        print("Usage: generar-compose.py <output_file> <number_of_clients>")
+        return None, None
+    
+    name = args[1]
+    try:
+        clients = int(args[2])
+        if clients < 1:
+            raise ValueError()
+    except ValueError:
+        print("The number of clients must be a positive integer")
+        return None, None
+
+    return name, clients
+
+def generate_server_lines():
+    return [
+        "  server:",
+        "    container_name: server",
+        "    image: server:latest",
+        "    entrypoint: python3 /main.py",
+        "    environment:",
+        "      - PYTHONUNBUFFERED=1",
+        "      - LOGGING_LEVEL=DEBUG",
+        "    networks:",
+        "      - testing_net",
+        ""
+    ]
+
+def generate_client_lines(clients_size):
+    client_lines = []
+    for i in range(clients_size):
+        client_lines += [
+            f"  client{i + 1}:",
+            f"    container_name: client{i + 1}",
+            "    image: client:latest",
+            "    entrypoint: /client",
+            "    environment:",
+            f"      - CLI_ID={i + 1}",
+            "      - CLI_LOG_LEVEL=DEBUG",
+            "    networks:",
+            "      - testing_net",
+            "    depends_on:",
+            "      - server",
+            ""
+        ]
+    return client_lines
+
+def generate_network_lines():
+    return [
+        "networks:",
+        "  testing_net:",
+        "    name: testing_net",
+        "    ipam:",
+        "      driver: default",
+        ""
+    ]
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
